9b7dbfe44a7525e53fa2ac458174653c28c691f2
Aleksandr <Alfa>
2024-07-27 20:22:48 +0300

    task #3 solution

---
+++ Envs/homework3.py
@@ -0,0 +1,22 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+
+# print_params(1,2,3,4,5)
+
+print_params(b=25)
+
+print_params(c=[1, 2, 3])
+
+
+values_list = [1, 'Boom', False]
+values_dict = {'a': 1,'b': 'Тип','c': 1.6}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
+
