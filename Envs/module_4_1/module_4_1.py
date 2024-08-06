0113d2b32f3d18c9445f8d7ad23e5d02874c8ee4
Aleksandr <Alfa>
2024-08-06 19:56:21 +0300

    add pack module_4_1

---
+++ Envs/module_4_1/module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fdiv
+from true_math import divide as tdiv
+
+first = 2
+second = 0
+print(fdiv(first, second))
+print(tdiv(first, second))
+
+first = 10
+second = 5
+print(fdiv(first, second))
+print(tdiv(first, second))
